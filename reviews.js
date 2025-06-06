aced9aad29a011b06119a0ace12c9c8a4e70de57
karen <karenwanngugi@gmail.com>
2025-06-06 21:32:38 +0300

    added images to image folder

--- reviews.js
+++ reviews.js
@@ -4,7 +4,7 @@ const reviews = [
         name: "John Rings",
         position: "Designer",
         img:
-            "John.jpeg",
+            "Images/John.jpeg",
         text:
             "The headphones were on. They had been utilized on purpose. She could hear her mom yelling in the background, but couldn't make out exactly what the yelling was above" 
     },{
@@ -12,7 +12,7 @@ const reviews = [
         name: "Joan Truman",
         position: "Designer",
         img:
-            "joan.jpg",
+            "Images/joan.jpg",
         text:
             "He wondered if he should disclose the truth to his friends. It would be a risky move. Yes, the truth would make things a lot easier if they all stayed on the same page, but the truth might fracture the group leaving everything in even more of a mess than it was not telling the truth" 
     },{
@@ -20,7 +20,7 @@ const reviews = [
         name: "Theodore James",
         position: "Actor",
         img:
-            "Theo.webp",
+            "Images/Theo.webp",
         text:
             "here are a number of reasons you may need a block of text and when you do, a random paragraph can be the perfect solution. If you happen to be a web designer and you need some random text to show in your layout, a random paragraph can be an excellent way to do this. " 
     },{
@@ -28,7 +28,7 @@ const reviews = [
         name: "Michael Jordan",
         position: "Basketball player ",
         img:
-            "Michael.jpg",
+            "Images/Michael.jpg",
         text:
             "For some writers, it isn't getting the original words on paper that's the challenge, but rewriting the first and second drafts. Using the random paragraph generator can be a good way to get into a rewriting routine before beginning the project." 
     }
